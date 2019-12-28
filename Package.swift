1e29bab158b7eb4e0fe705f7612bb500bdfba9cd
Trevör Anne Denise <trevor.annedenise@icloud.com>
2019-12-28 19:00:10 +0100

    Temp fix

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0-beta.2"),
-        .package(url: "https://github.com/vapor/sqlite-kit.git", from: "4.0.0-beta.2"),
+        .package(url: "https://github.com/adtrevor/sqlite-kit.git", from: "fixios"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
